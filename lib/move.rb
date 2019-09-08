7a218c4b0157c0936ce9fe481e1490ab6d2825b0
Alex Stolzoff <alexstolznut@hotmail.com>
2019-09-08 08:14:03 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,7 +9,7 @@ end
 # code your input_to_index and move method here!
 def input_to_index(index)
   index.to_i
-  if index>0 && index<10
+  if index > 0 && index < 10
     board[index-1].push(move)
   end
 
